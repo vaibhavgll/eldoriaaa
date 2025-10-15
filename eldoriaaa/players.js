2855f78df6ff0ef957116181e89504f8f75f734d
vaibhavgll <vgamer56588@gmail.com>
2025-10-16 03:40:18 +0530

    Create players.js

---
+++ eldoriaaa/players.js
@@ -0,0 +1,7 @@
+// The list of currently whitelisted players.
+// To add a new player, just add their Minecraft username inside the quotes.
+// To remove a player, just delete their line.
+const whitelistedPlayers = [
+  "VGAMER56588"
+  // Add your whitelisted players here
+];
